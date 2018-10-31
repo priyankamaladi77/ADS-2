8bc42f4cd7ebb956c986de2b8ef2ae9c9ff259a4
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-31 14:48:21 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m2/2.1 CC_Percolation/Solution.java
+++ Assignments/m2/2.1 CC_Percolation/Solution.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 /**
- * { Solution class }.
+ * class for solution.
  */
-final class Solution {
+public final class Solution {
     /**
-     * Constructs the object.
+     * Constructs the object for solution class.
      */
     private Solution() {
-        //constructor
+        // default constructor.
     }
     /**
-     * { main method }.
+     * main method.
+     * 
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nodes = Integer.parseInt(sc.nextLine());
-        int[][] matrix  = new int[nodes][nodes];
-        Graph graph = new Graph(nodes * nodes + 2);
+        int n = Integer.parseInt(sc.nextLine());
+        int[][] m  = new int[n][n];
+        Graph graph = new Graph(n * n + 2);
         while (sc.hasNext()) {
-            String[] loc = sc.nextLine().split(" ");
-            int row = Integer.parseInt(loc[0]);
-            int col = Integer.parseInt(loc[1]);
-            matrix[row - 1][col - 1] = 1;
+            String[] location = sc.nextLine().split(" ");
+            int row = Integer.parseInt(location[0]);
+            int col = Integer.parseInt(location[1]);
+            m[row - 1][col - 1] = 1;
         }
-        Percolation p = new Percolation();
-        System.out.println(p.check(matrix, graph, nodes));
+        Percolation per = new Percolation();
+        System.out.println(per.check(m, graph, n));
     }
 }
