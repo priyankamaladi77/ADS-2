6969d971d7b4e08641feb777a12ba728831a062c
priyankamaladi77 <svanmp@msitprogram.net>
2018-11-12 17:32:05 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m13/Radix Sorts/Solution.java
+++ Assignments/m13/Radix Sorts/Solution.java
@@ -6,30 +6,30 @@ import java.util.Scanner;
  * class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object for solution class.
-	 */
-	private Solution() {
-		//default constructor.
-	}
-	/**
-	 * main method for sorting the series of strings.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		Quick3string nums = new Quick3string();
-		String[] arr = new String[n];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextLine();
-		}
-		nums.sort(arr);
-		String str = "[";
-		for (String word:arr) {
-			str += word + ", ";
-		}
-		System.out.println(str.substring(0, str.length() - 2)  + "]");
-	}
+    /**
+     * Constructs the object for solution class.
+     */
+    private Solution() {
+        //default constructor.
+    }
+    /**
+     * main method for sorting the series of strings.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Quick3string nums = new Quick3string();
+        String[] arr = new String[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextLine();
+        }
+        nums.sort(arr);
+        String str = "[";
+        for (String word:arr) {
+            str += word + ", ";
+        }
+        System.out.println(str.substring(0, str.length() - 2)  + "]");
+    }
 }
